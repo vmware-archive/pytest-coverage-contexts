39c757cd5588b8ba5dfd1cf7667da9cfcdb1690a
Pedro Algarvio <pedro@algarvio.me>
2020-12-08 09:04:21 +0000

    Reset contexts

--- coveragectx/pytest/plugin.py
+++ coveragectx/pytest/plugin.py
@@ -30,57 +30,84 @@ class CoverageContextPlugin:
     address = attr.ib(init=False, default=None)
     running = attr.ib(init=False, default=False, hash=False)
 
-    @pytest.hookimpl
+    @pytest.hookimpl(hookwrapper=True)
     def pytest_collection(self):
         """
         Perform the collection phase for the given session.
         """
-        self.switch_context("collection")
+        try:
+            self.switch_context("collection")
+            yield
+        finally:
+            self.switch_context(None)
 
-    @pytest.hookimpl
+    @pytest.hookimpl(hookwrapper=True)
     def pytest_runtest_logstart(self, nodeid):
         """
         Called at the start of running the runtest protocol for a single item.
         """
-        self.switch_context("{}|start".format(nodeid))
+        try:
+            self.switch_context("{}|start".format(nodeid))
+            yield
+        finally:
+            self.switch_context(None)
 
-    @pytest.hookimpl
+    @pytest.hookimpl(hookwrapper=True)
     def pytest_runtest_setup(self, item):
         """
         Called to perform the setup phase for a test item.
         """
-        self.switch_context("{}|setup".format(item.nodeid))
+        try:
+            self.switch_context("{}|setup".format(item.nodeid))
+            yield
+        finally:
+            self.switch_context(None)
 
-    @pytest.hookimpl
+    @pytest.hookimpl(hookwrapper=True)
     def pytest_runtest_call(self, item):
         """
         Called to run the test for test item (the call phase).
         """
-        self.switch_context("{}|call".format(item.nodeid))
+        try:
+            self.switch_context("{}|call".format(item.nodeid))
+            yield
+        finally:
+            self.switch_context(None)
 
-    @pytest.hookimpl
+    @pytest.hookimpl(hookwrapper=True)
     def pytest_runtest_teardown(self, item):
         """
         Called to perform the teardown phase for a test item.
         """
-        self.switch_context("{}|teardown".format(item.nodeid))
+        try:
+            self.switch_context("{}|teardown".format(item.nodeid))
+            yield
+        finally:
+            self.switch_context(None)
 
-    @pytest.hookimpl
+    @pytest.hookimpl(hookwrapper=True)
     def pytest_runtest_logfinish(self, nodeid):
         """
         Called at the end of running the runtest protocol for a single item.
         """
-        self.switch_context("{}|finish".format(nodeid))
+        try:
+            self.switch_context("{}|finish".format(nodeid))
+            yield
+        finally:
+            self.switch_context(None)
 
     def switch_context(self, context):
         """
         Send the new dynamic context to PULL'ers
         """
-        log.debug("Switching to context: %s", context)
+        if context is None:
+            log.debug("Resetting context")
+            context = "[{NONE}]"
+        else:
+            log.debug("Switching context to: %s", context)
         os.environ["COVERAGE_DYNAMIC_CONTEXT"] = context
         if self.pusher is not None:
             self.pusher.send_string(context)
-        log.debug("Switched to context: %s", context)
 
     def start(self):
         """
