0a8cf70feaad69a538484d8c16154b75aefe96a9
Pedro Algarvio <pedro@algarvio.me>
2021-01-04 06:28:13 +0000

    Print the traceback to `sys.stderr` if `coverage.process_start()` raises exceptions

--- coveragectx/coverage/site/sitecustomize.py
+++ coveragectx/coverage/site/sitecustomize.py
@@ -4,9 +4,17 @@
 
 This module gets imported by Python's site machinery
 """
+import sys
+import traceback
+
 try:
     import coverage
 
     coverage.process_startup()
 except ImportError:
+    # We just ignore if coverage is not importable
     pass
+except Exception:  # pylint: disable=broad-except
+    traceback.print_exc(file=sys.stderr)
+    sys.stderr.flush()
+    raise
